0d22ab95605ccdaa29729423a9488418cd52b020
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2021-10-15 01:26:12 +0200

    ll

--- app/src/main/java/com/example/ketchappn/MainActivity.java
+++ app/src/main/java/com/example/ketchappn/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ketchappn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -11,20 +14,32 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore firestore;
     private CollectionReference collectionReference;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+/*
         firestore = FirebaseFirestore.getInstance();
         collectionReference = firestore.collection("Aktiviteterr");
         person p = new person("Fotball", "2");
         collectionReference.add(p);
+*/
+        setContentView(R. layout.fragment_login);
 
-        setContentView(R. layout.activity_main);
+        btn = findViewById(R.id.signInButton);
+/*
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
 
+            }
+        });*/
     }
+
+
 
     class person {
         private String name, email;
