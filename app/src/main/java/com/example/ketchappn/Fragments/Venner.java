3032ed96ed422124a5465a1ce12c3e7a3315b528
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2021-10-15 02:42:24 +0200

    yolo

--- app/src/main/java/com/example/ketchappn/Fragments/Venner.java
+++ app/src/main/java/com/example/ketchappn/Fragments/Venner.java
@@ -1,4 +1,4 @@
-package com.example.ketchappn.Fragments;
+package com.example.ketchappn.fragments;
 
 import android.os.Bundle;
 
@@ -10,52 +10,8 @@ import android.view.ViewGroup;
 
 import com.example.ketchappn.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Venner#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Venner extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Venner() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Venner.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Venner newInstance(String param1, String param2) {
-        Venner fragment = new Venner();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/com/example/ketchappn/Start_Page.java
+++ app/src/main/java/com/example/ketchappn/Start_Page.java
@@ -1,27 +1,33 @@
 package com.example.ketchappn;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
 import android.view.MenuItem;
 
 
-import com.example.ketchappn.Fragments.Aktivitet;
-import com.example.ketchappn.Fragments.Grupper;
-import com.example.ketchappn.Fragments.Minner;
-import com.example.ketchappn.Fragments.Venner;
+import com.example.ketchappn.fragments.Aktivitet;
+import com.example.ketchappn.fragments.Grupper;
+import com.example.ketchappn.fragments.Minner;
+import com.example.ketchappn.fragments.Venner;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class Start_Page extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     BottomNavigationView bottomNavigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.Venner_Navigation);
-
     }
 
     Minner fragment_minner = new Minner();
@@ -37,19 +43,19 @@ public class Start_Page extends AppCompatActivity implements BottomNavigationVie
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.Minner_Navigation:
-                getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, fragment_minner).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment_minner).commit();
                 return true;
 
             case R.id.Venner_Navigation:
-                getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, fragment_venner).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment_venner).commit();
                 return true;
 
             case R.id.Grupper_Navigation:
-                getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, fragment_grupper).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment_grupper).commit();
                 return true;
 
             case R.id.Aktivitet_Navigation:
-                getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout, fragment_aktivitet).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment_aktivitet).commit();
                 return true;
         }
 
