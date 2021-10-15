48a36e74cdd525f24d14ba293473c2725b9b66a6
Robin Cavlek <robin.cavlek.jobb@hotmail.com>
2021-10-15 20:26:27 +0200

    Aktivitetsskjerm. Bruker kan velge aktivitet så bli sendt til neste skjerm. Neste skjerm er ikke ferdiglaget enda.

--- app/src/main/java/com/example/ketchappn/MainActivity2.java
+++ app/src/main/java/com/example/ketchappn/MainActivity2.java
@@ -2,8 +2,16 @@ package com.example.ketchappn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,7 +22,36 @@ public class MainActivity2 extends AppCompatActivity {
 
         String aktivitet = getIntent().getStringExtra("Aktivitet");
 
+        TimePicker timePicker = (TimePicker) findViewById(R.id.datePicker1);
+        timePicker.setIs24HourView(true);
+
         TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setText(aktivitet);
+
+        EditText editText = (EditText) findViewById(R.id.editPlace);
+
+        Button button =(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int hour, minute;
+                String place;
+
+                hour = timePicker.getHour();
+                minute = timePicker.getMinute();
+                Editable editable = editText.getText();
+                place = editable.toString();
+
+                textView.setText("Selected time: "+ hour +":"+ minute + " " + place);
+            }
+        });
+
+        Button buttonBack = (Button) findViewById(R.id.button2);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
-}
+    }
