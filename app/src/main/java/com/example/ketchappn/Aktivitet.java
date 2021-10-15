dfa230bd60eb5a79c25cf7a983692bf4d2c4e4c6
Robin Cavlek <robin.cavlek.jobb@hotmail.com>
2021-10-15 06:20:42 +0200

    rrr

---
+++ app/src/main/java/com/example/ketchappn/Aktivitet.java
@@ -0,0 +1,49 @@
+package com.example.ketchappn;
+
+public class Aktivitet {
+    private String name, url;
+    private int id;
+
+
+    public Aktivitet(String name, String url, int id) {
+        this.name = name;
+        this.url = url;
+        this.id = id;
+    }
+
+    public Aktivitet() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "aktivitet{" +
+                "namewassup='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
--- app/src/main/java/com/example/ketchappn/MainActivity.java
+++ app/src/main/java/com/example/ketchappn/MainActivity.java
@@ -1,53 +1,174 @@
 package com.example.ketchappn;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.ketchappn.functions.FirestoreFunctions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseFirestore firestore;
+    private DocumentReference docReference;
     private CollectionReference collectionReference;
+    private ArrayList<Aktivitet> aktiviteter;
+    private FirestoreFunctions firestoreFunctions;
+
+    private TextView text;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         firestore = FirebaseFirestore.getInstance();
-        collectionReference = firestore.collection("Aktiviteterr");
-        person p = new person("Fotball", "2");
-        collectionReference.add(p);
+        aktiviteter = getMultipleDocs();
+
+        text = (TextView) findViewById(R.id.textView);
+
+
+        //setDocument("Volleyball", "jjs", 5);
+
 
         setContentView(R. layout.activity_main);
 
 
+
+
     }
 
-    class person {
-        private String name, email;
 
-        public person(String name, String email){
-            this.name = name;
-            this.email = email;
-        }
+    public void onClick(View v) {
 
-        public String getName() {
-            return name;
-        }
+       generateSymbols();
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
+        text = (TextView) findViewById(R.id.textView);
+        if (v.getId() == R.id.imageButton1) {
+            text.setText(String.valueOf(aktiviteter.size()));
         }
     }
-}
+
+    //Lager knapper for hver aktivitet vi har registrert i databasen
+    public void generateSymbols() {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.rootlayout);
+        for (int i = 0; i < aktiviteter.size(); i++){
+            Button btn = new Button(this);
+            btn.setText(String.valueOf(aktiviteter.get(i).getName()));
+            btn.setBackgroundResource(R.drawable.ball);
+            int act = i;
+            btn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    text = (TextView) findViewById(R.id.textView);
+                    text.setText(String.valueOf(aktiviteter.get(act).getName()));
+                }
+            });
+            btn.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout.addView(btn);
+            Log.d("JA", String.valueOf(aktiviteter.get(i)));
+        }
+    }
+
+
+
+
+    public void getDocument(){
+        DocumentReference docRef = firestore.collection("Aktiviteter").document("Fotball");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            private static final String TAG = "TAG";
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    public ArrayList<Aktivitet> getMultipleDocs() {
+        ArrayList<Aktivitet> array = new ArrayList<>();
+
+        // [START get_multiple]
+        firestore.collection("Aktiviteter")
+                .whereGreaterThanOrEqualTo("id", 0)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                Aktivitet aktivitet = document.toObject(Aktivitet.class);
+                                array.add(aktivitet);
+                            }
+                        } else {
+                            Log.d("TAG", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        return array;
+    }
+
+    public void createObjectFromFB() {
+        DocumentReference docRef = firestore.collection("Aktiviteter").document("Fotball");
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Aktivitet aktivitet = documentSnapshot.toObject(Aktivitet.class);
+                Log.d("TAG", aktivitet.toString());
+            }
+        });
+    }
+
+    public void setDocument(String name, String url, int id) {
+
+        Map<String, Object> aktivitet = new HashMap<>();
+        aktivitet.put("name", name);
+        aktivitet.put("url", url);
+        aktivitet.put("id", id);
+
+        firestore.collection("Aktiviteter").document(name)
+                .set(aktivitet)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG", "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TAG", "Error writing document", e);
+                    }
+                });
+}}
