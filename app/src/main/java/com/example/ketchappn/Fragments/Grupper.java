c5bd1b19c407a896d2633b57287a996c7bdcfc5b
AleksanderVEriksen <70018977+AleksanderVEriksen@users.noreply.github.com>
2021-12-12 19:19:13 +0100

    Finpusser

--- app/src/main/java/com/example/ketchappn/Fragments/Grupper.java
+++ app/src/main/java/com/example/ketchappn/Fragments/Grupper.java
@@ -10,11 +10,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.Source;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -27,7 +25,6 @@ import android.view.View;
 
 
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 
 import java.util.ArrayList;
@@ -37,13 +34,15 @@ import java.util.List;
 
 public class Grupper extends Fragment  {
     private static final String ARRAGEMENTER = "Arrangement";
+    private static final String USER = "User";
 
     ArrayList<QueryDocumentSnapshot> list;
+    ArrayList<String> JoinedActivity_names;
 
     RecyclerView recyclerView;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference database = db.collection(ARRAGEMENTER);
-    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();;
+    CollectionReference databaseUser = db.collection(USER);
 
     View view;
 
@@ -57,8 +56,24 @@ public class Grupper extends Fragment  {
         recyclerView = view.findViewById(R.id.recyclerview_GRUPPE);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
+        databaseUser.get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    ArrayList<HashMap<String, Object>> activity = (ArrayList<HashMap<String, Object>>) document.get("JoinedActivity");
+                    //Log.d("GRUPPER_SOM_SKAL_VISES","Du har email: " + LoginAct.CurUser.getEmail());
+                    if (activity != null && LoginAct.CurUser.getEmail().equals(document.getId())) {
+                        for (int i = 0; i < activity.size(); i++) {
+                            Log.d(USER, " Email: " + document.getId() + " => " + activity.get(i).get("Name"));
+                            // JoinedActivity_names.add((String) activity.get(i).get("Name"));
+                        }
+                    }
 
-        CollectionReference users = firestore.collection("User");
+                }
+            }
+            else{
+                Log.d(USER,"Error ",task.getException());
+            }
+        });
         list = new ArrayList<>();
 
         database.get()
@@ -66,10 +81,10 @@ public class Grupper extends Fragment  {
                     if(task.isSuccessful()) {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             List<String> venner = (List<String>) document.get("venner");
-                            Log.d("GRUPPER_SOM_SKAL_VISES","Du har email: " + LoginAct.CurUser.getEmail());
+                            //Log.d("GRUPPER_SOM_SKAL_VISES","Du har email: " + LoginAct.CurUser.getEmail());
                             if (venner != null) {
                                 for (int i = 0; i < venner.size(); i++) {
-                                    Log.d(ARRAGEMENTER, " => " + venner.get(i));
+                                    //Log.d(ARRAGEMENTER, " => " + venner.get(i));
                                     if (LoginAct.CurUser.getEmail().equals(venner.get(i)) || LoginAct.CurUser.getEmail().equals(document.get("host").toString())) {
                                         list.add(document);
                                         recyclerView.setAdapter(new recyclerAdapter(getContext(), list));
@@ -81,7 +96,7 @@ public class Grupper extends Fragment  {
                         }
                     }
                     else{
-                        Log.d("Nigga","Fisk",task.getException());
+                        Log.d(ARRAGEMENTER,"Error ",task.getException());
                     }
                 });
         return view;
--- app/src/main/java/com/example/ketchappn/recyclerViewHolder/recyclerAdapter.java
+++ app/src/main/java/com/example/ketchappn/recyclerViewHolder/recyclerAdapter.java
@@ -40,7 +40,7 @@ public class recyclerAdapter extends RecyclerView.Adapter<recyclerAdapter.recycl
     public void onBindViewHolder(@NonNull recyclerviewholder viewholder, int position) {
         HashMap<String, Object> d = (HashMap<String, Object>) list.get(position).get("aktivitet");
         QueryDocumentSnapshot bd  = list.get(position);
-        Log.d("bindholder ", " => " + d.get("name").toString());
+        //Log.d("bindholder ", " => " + d.get("name").toString());
         viewholder.sted.setText(bd.get("sted").toString());
         viewholder.symbol.setText(d.get("symbol").toString());
         viewholder.tid.setText(bd.get("tid").toString());
