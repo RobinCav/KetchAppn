a2f18ecf1246132b9a8ff0000c30b2cb2a1c5987
Robin Cavlek <robin.cavlek.jobb@hotmail.com>
2021-12-14 01:26:36 +0100

    GroupChat yeye

--- app/src/main/java/com/example/ketchappn/Fragments/Grupper.java
+++ app/src/main/java/com/example/ketchappn/Fragments/Grupper.java
@@ -1,5 +1,4 @@
 
-
 package com.example.ketchappn.Fragments;
 import com.example.ketchappn.GroupChatActivity;
 import com.example.ketchappn.R;
@@ -40,14 +39,18 @@ import java.util.List;
 
 public class Grupper extends Fragment  {
     private static final String ARRAGEMENTER = "Arrangement";
+    private static final String USER = "User";
 
     ArrayList<QueryDocumentSnapshot> list;
 
-    RecyclerView recyclerView;
+    RecyclerView  recyclerView;
     LinearLayout linearLayout;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference database = db.collection(ARRAGEMENTER);
-    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();;
+
+    ArrayList<String> JoinedActivity_names;
+
+    CollectionReference databaseUser = db.collection(USER);
 
     View view;
 
@@ -67,34 +70,53 @@ public class Grupper extends Fragment  {
 
         linearLayout = view.findViewById(R.id.linearLayout_gruppe);
 
-
-        CollectionReference users = firestore.collection("User");
         list = new ArrayList<>();
+        JoinedActivity_names = new ArrayList<>();
 
-        database.get()
-                .addOnCompleteListener(task -> {
-                    if(task.isSuccessful()) {
-                        list.clear();
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            List<String> venner = (List<String>) document.get("venner");
-                            System.out.println("Du har email: " + LoginAct.CurUser.getEmail());
-                            if (venner != null) {
-                                for (int i = 0; i < venner.size(); i++) {
-                                    Log.d(ARRAGEMENTER, " => " + venner.get(i));
-                                    if (LoginAct.CurUser.getEmail().equals(venner.get(i)) || LoginAct.CurUser.getEmail().equals(document.get("host").toString())) {
-                                        list.add(document);
-                                        recyclerView.setAdapter(new recyclerAdapter(getContext(), list, this));
-                                        break;
+        databaseUser.get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    ArrayList<HashMap<String, Object>> joinedActivity = (ArrayList<HashMap<String, Object>>) document.get("JoinedActivity");
+
+                    if (joinedActivity != null && LoginAct.CurUser.getEmail().equals(document.getId())) {
+                        for (int i = 0; i < joinedActivity.size(); i++) {
+                            Log.d(USER, " Email: " + document.getId() + " => " + joinedActivity.get(i).get("Name").toString());
+                            if(joinedActivity.get(i).get("Name") != null) {
+                                JoinedActivity_names.add((joinedActivity.get(i).get("Name").toString()));
+                                Log.d("Joined Activity names for active user: ", " => " + JoinedActivity_names);
+
+                            }
+                        }
+                    }
+
+                }
+            }
+            else{
+                Log.d(USER,"Error ",task.getException());
+            }
+            database.get()
+                    .addOnCompleteListener(task_Arrangement -> {
+                        if(task_Arrangement.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document_Arrangement : task_Arrangement.getResult()) {
+                                if(JoinedActivity_names != null) {
+                                    for (int h = 0; h < JoinedActivity_names.size(); h++) {
+                                        if (document_Arrangement.getId().equals(JoinedActivity_names.get(h))) {
+                                            //Log.d("GRUPPER_SOM_SKAL_VISES", "Arrangement: " + JoinedActivity_names.size() + " " + document_Arrangement.getId() + " = " + JoinedActivity_names.get(h));
+                                            list.add(document_Arrangement);
+                                            recyclerView.setAdapter(new recyclerAdapter(getContext(), list, this));
+                                            break;
+                                        }
+
                                     }
                                 }
                             }
-
                         }
-                    }
-                    else{
-                        Log.d("Nigga","Fisk",task.getException());
-                    }
-                });
+                        else{
+                            Log.d(ARRAGEMENTER,"Error ",task_Arrangement.getException());
+                        }
+                    });
+        });
 
         return view;
     }
